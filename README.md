03bfb5a8a319478ee508783ebeb1c09350602372
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-01 23:49:15 +0300

    optimization image component

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Resources
 
 - [next optimizing/images](https://nextjs.org/docs/app/building-your-application/optimizing/images)
-# next-optimization
+- [next images#priority](https://nextjs.org/docs/app/building-your-application/optimizing/images#priority)
+- [next image#fill](https://nextjs.org/docs/app/api-reference/components/image#fill)
+- [next image#loader](https://nextjs.org/docs/app/api-reference/components/image#loader)
--- components/posts.js
+++ components/posts.js
@@ -1,18 +1,23 @@
-"use client";
+'use client';
 
 import { useOptimistic } from 'react';
 
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
+import Image from 'next/image';
 
 function Post({ post, action }) {
+  function imageLoader(config) {
+    console.log(config);
+    return config.src;
+  }
   return (
-    <article className="post">
-      <div className="post-image">
-        <img src={post.image} alt={post.title} />
+    <article className='post'>
+      <div className='post-image'>
+        <Image loader={imageLoader} src={post.image} fill alt={post.title} />
       </div>
-      <div className="post-content">
+      <div className='post-content'>
         <header>
           <div>
             <h2>{post.title}</h2>
@@ -39,20 +44,25 @@ function Post({ post, action }) {
 }
 
 export default function Posts({ posts }) {
-  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
-    const updatedPostIndex = prevPosts.findIndex(post => post.id === updatedPostId);
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
 
-    if (updatedPostIndex === -1) {
-      return prevPosts;
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+      return newPosts;
     }
-
-    const updatedPost = { ...prevPosts[updatedPostIndex] };
-    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
-    updatedPost.isLiked = !updatedPost.isLiked;
-    const newPosts = [...prevPosts];
-    newPosts[updatedPostIndex] = updatedPost;
-    return newPosts;
-  })
+  );
 
   if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
@@ -64,7 +74,7 @@ export default function Posts({ posts }) {
   }
 
   return (
-    <ul className="posts">
+    <ul className='posts'>
       {optimisticPosts.map((post) => (
         <li key={post.id}>
           <Post post={post} action={updatePost} />
